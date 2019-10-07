d438c1709bbca40df3b2f55d41b1bbce11da3173
Your <Your.name@yourmail.com>
2019-10-08 00:16:41 +0100

    more of the methods

--- Enumerable_Methods.rb
+++ Enumerable_Methods.rb
@@ -70,4 +70,79 @@ module Enumerable
         output
     end
 
-    
+    def my_none? #my_none methods
+        output = true
+        self.my_each do |b|
+            if yield (b)
+                output = false
+                break
+            end
+        end
+        output
+    end
+
+    def my_count parameter = nil #my_count method
+        mycount = 0
+        self.my_each do |b|
+            if parameter != nil 
+                if parameter == self[b]
+                    mycount += 1
+                end
+            else
+                if yield (b)
+                    mycount += 1
+                    self.my_each do |element|
+                        if parameter
+                            if element == parameter
+                                mycount += 1
+                            end
+                        end
+                    elsif block_given?
+                        mycount += if yield(element)
+                        else
+                            mycount = self.length
+                        end
+                    end
+                    mycount
+                end
+
+    def my_map #my_map method
+        mymap = []
+        self.my_each do |b|
+            mymap.push yield(b)
+        end
+        mymap
+    end
+
+    def my_map_again(myproc) #my_mapa_gain method which takes a proc
+        self.my_each do |b|
+        mymapagain.push myproc.call(b)
+        end
+        mymapagain
+    end
+
+    def my_map_final myproc = nil #my_map_final method which an either take a proc or a block
+        mymapfinal = []
+        self.my_each do |b|
+            if myproc == nil 
+                mymapfinal.push yield(b)
+            else
+                mymapfinal.push myproc.call(b)
+            end 
+        end
+        mymapfinal
+    end
+
+    def my_inject merit = 0 #my_inject method
+        myinjet merit
+        self.my_each do |immediately|
+            myinject = yield myinject immediately
+
+        end
+        myinject
+    end
+end
+
+
+
+
